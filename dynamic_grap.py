428867f854433e3e4cc296e35d12893997c1676b
xiaoma5 <xiaoma5@illinois.edu>
2019-02-22 10:33:02 -0600

    Plot mutiple years GDP

--- dynamic_grap.py
+++ dynamic_grap.py
@@ -6,23 +6,26 @@ def read_data():
     filename = "API_NY.GDP.MKTP.CD_DS2_en_csv_v2_10475113/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_10475113.csv"
     Data = pd.read_csv(filename,skiprows=4)
     return Data
-def plot_bar(label, num_movies):
+def plot_bar(label, num_movies, Year):
     index = np.arange(len(label))
     plt.bar(index,num_movies)
     plt.xlabel('Country Name',fontsize = 5)
     plt.ylabel('GDP',fontsize = 5)
     plt.xticks(index,label,fontsize =5, rotation = 30)
-    plt.title('GDP for Countries in 2016')
+    plt.title('GDP for Countries in ' + Year)
     plt.show()
-    
-Data = read_data();
-Data = Data.sort_values('2016',ascending=False)
-Data = Data.head(n=40)
-label = Data['Country Name']
-print(type(label))
-num_movies = Data['2016']
-print(type(num_movies))
-print(Data.shape)
-print(label.shape)
-print(num_movies.shape)
-plot_bar(label, num_movies)
+
+def Data_select(Data, Year, num_countries):
+    Data = Data.sort_values(Year,ascending=False)
+    Data = Data.head(n=num_countries)
+    label = Data['Country Name']
+    num_movies = Data[Year]
+    return label, num_movies
+
+Years = ['2015','2016','2017']
+
+for i in range(len(Years)):
+    Data = read_data()
+    num_of_countries = 5
+    label, num_movies = Data_select(Data, Years[i],num_of_countries)
+    plot_bar(label,num_movies,Years[i])

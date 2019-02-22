0c9d6933be3d6db1a8e6aeb3ee7fb696c2d6f888
xiaoma5 <xiaoma5@illinois.edu>
2019-02-22 12:35:43 -0600

    draft layout finish, detail need to be improved

--- dynamic_grap.py
+++ dynamic_grap.py
@@ -7,18 +7,18 @@ def read_data():
     Data = pd.read_csv(filename,skiprows=4)
     return Data
 def plot_bar(label, num_movies, Year):
+    color_list =['r','g','b','c','m','y','k','Olive','burlywood','chartreuse']
     index = np.arange(len(label))
     fig1 = plt.figure(1)
     plt.clf()
-    #plt.ylim((0, 1e14))
-    plt.bar(index,num_movies)
-    plt.xlabel('Country Name',fontsize = 5)
-    plt.ylabel('GDP',fontsize = 5)
-    plt.xticks(index,label,fontsize =5, rotation = 30)
-    plt.title('GDP for Countries in ' + Year)
+    barlist = plt.barh(index,num_movies/1e9,color =color_list)
+    plt.ylabel('Country Name',fontsize = 5)
+    plt.xlabel('GDP',fontsize = 5)
+    plt.yticks(index,label,fontsize =5, rotation = 0)
+    plt.title('Wolrd GDP by Country  in Billions USD ' + 'Year ' + Year  )
     plt.pause(0.5)
+    plt.grid(True)
     fig1.canvas.draw()
-    #plt.show()
 
 def Data_select(Data, Year, Country_List):
     Data = Data.sort_values(Year,ascending=False)
@@ -28,13 +28,10 @@ def Data_select(Data, Year, Country_List):
     num_movies = Data[Year]
     return label, num_movies
 
-
 Data = read_data()
 Data.index = Data['Country Name']
 Country_List = ["United States", "United Kingdom", "France", "China", "Japan", "Canada","Italy", "India","Australia","Brazil"]
 Years = list(Data)[4:-2]
-print(Years)
 for i in range(len(Years)):
-    num_of_countries = 1
     label, num_movies = Data_select(Data, Years[i],Country_List)
     plot_bar(label,num_movies,Years[i])
